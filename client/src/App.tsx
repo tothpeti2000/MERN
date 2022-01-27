6759ac1a741a4620e2d45ff18fe86cb300aeca41
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-01-27 21:59:26 +0100

    EmployeeList added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,9 @@
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import * as React from "react";
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import EmployeeList from "./components/EmployeeList";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    <EmployeeList />
   </ChakraProvider>
 );
