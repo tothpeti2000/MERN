d31bb657c779c3c1c37276a50442a67cee883c56
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-01-27 15:41:36 +0100

    Express setup added

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const routes = require("./routes/record");
+const dbo = require("./db/conn");
+
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, () => {
+  dbo.ConnectToServer((error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+
+  console.log(`Server running at http://localhost:${PORT}`);
+});
