c7b6046ef3447ff5b0f6551c78b66246d4546d72
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-01-29 19:50:19 +0100

    Employee list fetch added

--- client/src/components/EmployeeList.tsx
+++ client/src/components/EmployeeList.tsx
@@ -1,9 +1,30 @@
 import { Flex } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import Employee from "../interfaces/Employee";
 import EmployeeCard from "./EmployeeCard";
 
 const EmployeeList = () => {
+  const [employees, setEmployees] = useState<Employee[]>([]);
+
+  const FetchEmployees = async () => {
+    const response = await fetch("http://localhost:5000/employees");
+    const data = await response.json();
+
+    return data;
+  };
+
+  const GetData = async () => {
+    const employees = await FetchEmployees();
+    setEmployees(employees);
+  };
+
+  useEffect(() => {
+    GetData();
+  }, []);
+
   return (
     <Flex w="100%" justifyContent="space-around" flexWrap="wrap">
+      {employees}
       <EmployeeCard />
       <EmployeeCard />
       <EmployeeCard />
